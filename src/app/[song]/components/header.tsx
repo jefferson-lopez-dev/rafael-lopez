649fa679a5225029f29c22d40582e571d9e3f231
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-06-09 13:24:28 -0600

    ya se puede descargar la musica

--- src/app/[song]/components/header.tsx
+++ src/app/[song]/components/header.tsx
@@ -1,14 +1,21 @@
-import { ChevronLeft, Download } from "lucide-react";
 import Link from "next/link";
+import { ChevronLeft, Download } from "lucide-react";
+import { downloadSong } from "../utils";
+import { Song } from "@/data";
 
-export function Header() {
+export function Header({ song }: { song: Song }) {
   return (
     <div className="flex justify-between items-center w-full">
       <Link href={"/"}>
         <ChevronLeft className="stroke-1 relative right-2 w-8 h-8 cursor-pointer" />
       </Link>
       <p className="text-xs opacity-70">REPRODUCTOR</p>
-      <Download className="stroke-1 cursor-pointer " />
+      <Download
+        onClick={async () => {
+          await downloadSong(song);
+        }}
+        className="stroke-1 cursor-pointer "
+      />
     </div>
   );
 }
