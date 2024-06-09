1620f92d9eee5d3693acf0294daafc6d0c40ffda
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-06-09 13:25:22 -0600

    se agregaron compativilidades entrelos paquetes

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Obtener el valor de __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -8,6 +15,18 @@ const nextConfig = {
         port: "",
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "react-native-fs": false, // Desactivar react-native-fs
+      jsmediatags: path.resolve(
+        __dirname,
+        "node_modules/jsmediatags/dist/jsmediatags.min.js"
+      ), // Forzar el uso de la versión del navegador
+    };
+
+    return config;
   },
 };
 
