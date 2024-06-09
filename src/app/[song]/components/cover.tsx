8d9b1947d9ae650d34cdd7a93f0a5e688f23a1d9
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-06-09 13:22:47 -0600

    se mejoro el cover y su formato

--- src/app/[song]/components/cover.tsx
+++ src/app/[song]/components/cover.tsx
@@ -18,46 +18,47 @@ export function Cover({ togglePlayPause, isPlay, slug, imagen }: Props) {
       className={`pt-3 py-10 pb-1 h-[200px] sm:h-[250px] justify-center flex flex-col gap-2 px-1`}
     >
       <motion.div
-        layout
         onClick={togglePlayPause}
-        className={clsx(
-          "relative group cursor-pointer w-full h-full rounded-md",
-          { "right-3": isPlay }
-        )}
+        layout
+        className="relative flex items-center justify-center"
       >
         <motion.div
           layout
-          className={clsx(
-            "absolute w-full h-full justify-center items-center",
-            { "left-[75px]": isPlay }
-          )}
+          className="z-10 absolute"
+          animate={isPlay ? { rotate: [0, 360] } : { rotate: 0 }}
+          transition={
+            isPlay ? { repeat: Infinity, duration: 2, ease: "linear" } : {}
+          }
         >
           <Image
             className={clsx(
-              "aspect-square transition-all brightness-50 contrast-100 rounded-md",
+              "shadow-inner w-[90px] aspect-square object-cover rounded-full shadow-black transition-all z-10"
+            )}
+            src={imagen}
+            alt={slug}
+            width={100}
+            height={100}
+          />
+        </motion.div>
+        <motion.div
+          layout
+          animate={isPlay ? { rotate: [0, 360] } : { rotate: 0 }}
+          transition={
+            isPlay ? { repeat: Infinity, duration: 2, ease: "linear" } : {}
+          }
+        >
+          <Image
+            className={clsx(
+              "aspect-square transition-all brightness-50 contrast-100 z-0 rounded-md",
               {
-                "w-[180px] sm:w-[230px]": !isPlay,
-                "w-[190px] sm:w-[240px]": isPlay,
-                "animate-spin-slow": isPlay,
+                "w-[230px]": !isPlay,
+                "w-[240px]": isPlay,
               }
             )}
             src={dics}
             alt={slug}
           />
         </motion.div>
-        <motion.img
-          className={clsx(
-            "shadow-2xl aspect-square contrast-125 object-cover rounded-md shadow-black transition-all relative",
-            {
-              "w-[180px] sm:w-[230px]": !isPlay,
-              "w-[190px] sm:w-[240px]": isPlay,
-              "brightness-75": !isPlay,
-              "brightness-100": isPlay,
-            }
-          )}
-          src={imagen}
-          alt={slug}
-        />
       </motion.div>
     </div>
   );
