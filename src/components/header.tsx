20e9bbbc98422b0b25c3870890092d6736ab34e3
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-06-12 10:33:34 -0600

    cambio del nombre del header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,10 @@
 export function Header() {
   return (
-    <header className="flex flex-col w-full items-center py-10">
+    <header className="flex flex-col w-full items-center gap-4 py-10">
       <h1 className="font-cursive text-7xl sm:text-8xl">Rafael Lopez</h1>
+      <span className="font-cursive text-neutral-300 text-5xl">
+        El Rondoneño
+      </span>
     </header>
   );
 }
