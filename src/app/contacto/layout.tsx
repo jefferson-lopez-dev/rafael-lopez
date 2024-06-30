74a04e62f3c68c8d8a09fd1fd739d4e2abd54b52
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-06-30 14:56:30 -0600

    se mejoro la metadata

--- src/app/contacto/layout.tsx
+++ src/app/contacto/layout.tsx
@@ -6,6 +6,9 @@ import { ReactNode } from "react";
 export const metadata: Metadata = {
   title: "Contacto - Rafael Lopez",
   description: "Sigueme en mis redes sociales",
+  openGraph: {
+    images: "/jose_rafael_lopez_1.jpg",
+  },
 };
 
 export default function LayoutContacto({ children }: { children: ReactNode }) {
