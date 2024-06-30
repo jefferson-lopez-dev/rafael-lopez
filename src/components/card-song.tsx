b712303eb2a54ab661eb45aad404c14429ab9740
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-06-30 12:52:32 -0600

    se mejoro la card de la cancion

--- src/components/card-song.tsx
+++ src/components/card-song.tsx
@@ -3,40 +3,33 @@ import { Play } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-export function CardSong({ title, description, time, cover, slug }: Song) {
+export function CardSong({ title, cover, slug, album }: Song) {
   return (
     <Link
       href={`/${slug}`}
-      className="flex p-5 rounded-md max-sm:bg-neutral-950 group sm:bg-gradient-to-r sm:from-transparent sm:via-neutral-900/65 sm:to-transparent transition-all max-sm:flex-col cursor-pointer justify-between gap-3"
+      className="flex px-5 py-2 rounded-md max-sm:bg-neutral-950 group sm:bg-gradient-to-r sm:from-transparent sm:via-neutral-900/65 sm:to-transparent transition-all max-sm:flex-col cursor-pointer justify-between gap-3"
     >
-      <div className="flex justify-between">
-        <div className="w-24 min-w-24 flex items-center justify-center bg-white h-24 rounded-[7px]">
-          <Image
-            className="aspect-square object-cover rounded-md"
-            src={cover}
-            alt={slug}
-            width={96}
-            height={96}
-          />
+      <div className="flex relative w-full gap-2 items-center justify-between">
+        <div className="flex gap-2 items-center">
+          <div>
+            <Image
+              className="aspect-square min-w-[50px] object-cover rounded-md"
+              src={cover}
+              alt={slug}
+              width={50}
+              height={50}
+            />
+          </div>
+          <div className="flex flex-col">
+            <span className="w-full text-ellipsis overflow-hidden whitespace-nowrap ">
+              {title}
+            </span>
+            <span className="text-sm opacity-70">{album}</span>
+          </div>
         </div>
-        <div className="w-10 min-w-10 sm:hidden flex items-center justify-center h-10 rounded-full bg-white">
-          <Play className="text-black fill-black" />
+        <div className="w-8 min-w-8 absolute right-0 flex items-center justify-center h-8 rounded-full bg-white">
+          <Play className="text-black w-5 h-5 fill-black" />
         </div>
-      </div>
-      <div className="flex flex-col w-full justify-between items-start h-24">
-        <div>
-          <span className="font-medium sm:font-bold sm:text-xl">{title}</span>
-          <p className="text-sm max-w-[400px] text-neutral-200">
-            {description &&
-              description
-                .slice(0, 110)
-                .concat(description?.length > 109 ? "..." : "")}
-          </p>
-        </div>
-        <span className="text-sm font-semibold">{time}</span>
-      </div>
-      <div className="w-10 group-hover:opacity-100 opacity-0 max-sm:hidden min-w-10 flex items-center justify-center h-10 rounded-full bg-white">
-        <Play className="text-black fill-black" />
       </div>
     </Link>
   );
