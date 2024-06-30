0a78ddc60bb9e807334982b6d652bd87d9b655f2
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-06-30 14:53:59 -0600

    cambio de la metadata

--- src/app/contacto/layout.tsx
+++ src/app/contacto/layout.tsx
@@ -1,6 +1,12 @@
 import { Header } from "@/components";
 import { Navigation } from "@/components/navigation";
+import { Metadata } from "next";
 import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "Contacto - Rafael Lopez",
+  description: "Sigueme en mis redes sociales",
+};
 
 export default function LayoutContacto({ children }: { children: ReactNode }) {
   return (
