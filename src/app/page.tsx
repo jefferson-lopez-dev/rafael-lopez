3b2772e6e338316d5cafa1e218836ae26b0bc000
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-07-02 15:59:41 -0600

    proof mart fect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import { Header, SongsList } from "@/components";
 import { Navigation } from "@/components/navigation";
 import { SongBackground } from "@/components/song-background";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch("http://localhost:3000/api/session/get-token", {
+    headers: headers(),
+  });
+
   return (
     <main className="px-5 min-h-[90dvh] gap-4 flex flex-col items-center select-none">
       <Header />
