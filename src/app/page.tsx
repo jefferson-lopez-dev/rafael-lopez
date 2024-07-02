b9af9f06701cd0a198b93dd78e155ff22380c0e1
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-07-02 16:46:07 -0600

    delete proof

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
 import { Header, SongsList } from "@/components";
 import { Navigation } from "@/components/navigation";
 import { SongBackground } from "@/components/song-background";
-import { headers } from "next/headers";
 
-export default async function Home() {
-  const res = await fetch("http://localhost:3000/api/session/get-token", {
-    headers: headers(),
-  });
-  const data = await res.json();
-  console.log(data);
-
+export default function Home() {
   return (
     <main className="px-5 min-h-[90dvh] gap-4 flex flex-col items-center select-none">
       <Header />
