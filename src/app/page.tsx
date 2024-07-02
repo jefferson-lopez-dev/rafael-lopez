36860c8e41fbb9ef91440f29bbd93ad5895e2fab
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-07-02 16:00:05 -0600

    get headers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Header, SongsList } from "@/components";
 import { Navigation } from "@/components/navigation";
 import { SongBackground } from "@/components/song-background";
+import { headers } from "next/headers";
 
 export default async function Home() {
   const res = await fetch("http://localhost:3000/api/session/get-token", {
