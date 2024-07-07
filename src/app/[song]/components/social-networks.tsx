4cb3b2cb91a8b33939d19bee836200b44bc61d1d
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-07-07 13:42:14 -0600

    se cambio las redes sociales por las plataformas en donde puede escuhar la musica

--- src/app/[song]/components/social-networks.tsx
+++ src/app/[song]/components/social-networks.tsx
@@ -1,29 +1,35 @@
-import Link from "next/link";
+"use client";
+
+import { useMusica } from "@/context/musica";
 import Image from "next/image";
-import facebook from "../../../../public/facebook-icon.svg";
+import Link from "next/link";
 import youtube from "../../../../public/youtube-icon.svg";
 
 export function SocialNetworks() {
+  const { currentMusica, currentTimeSeconds, togglePlayPause, isPlay } =
+    useMusica();
+
   return (
     <div className="flex bg-neutral-900 rounded-lg flex-col">
-      <span className="text-sm px-3 opacity-75 pt-3">Redes Sociales</span>
+      <span className="text-sm px-3 opacity-75 pt-3">Plataformas</span>
       <div className="flex gap-3 items-center p-3">
-        <Link
-          target="_blank"
-          href={"https://www.facebook.com/profile.php?id=100063642931231"}
-          className="flex cursor-pointer border-neutral-700 items-center gap-1 border p-2 pl-3 px-4 rounded-full"
-        >
-          <Image src={facebook} alt="as" width={30} height={30} />
-          <span className="text-sm">Facebook</span>
-        </Link>
-        <Link
-          target="_blank"
-          href={"https://www.youtube.com/@joserafaellopezosto8627/videos"}
-          className="flex cursor-pointer border-neutral-700 items-center gap-1 border p-2 pl-3 px-4 rounded-full"
-        >
-          <Image src={youtube} alt="as" width={30} height={30} />
-          <span className="text-sm">YouTube</span>
-        </Link>
+        {currentMusica.platform_links.youtube && (
+          <Link
+            onClick={() => {
+              if (isPlay) {
+                togglePlayPause();
+              }
+            }}
+            target="_blank"
+            href={`${
+              currentMusica.platform_links.youtube
+            }&t=${currentTimeSeconds.toFixed()}s`}
+            className="flex cursor-pointer border-neutral-700 items-center gap-2 border p-2 pl-3 px-4 rounded-full"
+          >
+            <Image src={youtube} alt="as" width={30} height={30} />
+            <span className="text-sm">Escuchar En Youtube</span>
+          </Link>
+        )}
       </div>
     </div>
   );
