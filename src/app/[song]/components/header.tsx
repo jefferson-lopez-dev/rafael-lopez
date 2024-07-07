90e45f25de2610410e5f730d9ae2077c9eb4d2d9
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-07-07 13:41:00 -0600

    se cambiaron los icnos

--- src/app/[song]/components/header.tsx
+++ src/app/[song]/components/header.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { ChevronLeft, Download } from "lucide-react";
+import { ArrowLeft, ChevronLeft, Download } from "lucide-react";
 import { downloadSong } from "../utils";
 import { Song } from "@/data";
 
@@ -7,7 +7,7 @@ export function Header({ song }: { song: Song }) {
   return (
     <div className="flex justify-between items-center w-full">
       <Link href={"/"}>
-        <ChevronLeft className="stroke-1 relative right-2 w-8 h-8 cursor-pointer" />
+        <ArrowLeft className="stroke-1 relative cursor-pointer" />
       </Link>
       <p className="text-xs opacity-70">REPRODUCTOR</p>
       <Download
