7d38956e58c10051563ea48ba9d2dde27e04b446
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-07-07 14:30:30 -0600

    mejoras en el footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,19 +1,58 @@
-import { ExternalLink } from "lucide-react";
+/* eslint-disable @next/next/no-img-element */
+import { Facebook, Youtube } from "lucide-react";
 
 export function Footer() {
   return (
-    <footer className="w-full flex items-center justify-center bg-neutral-950 h-[10dvh]">
-      <span className="text-sm flex gap-1">
-        Pagina creada por{" "}
-        <a
-          target="_blank"
-          href="https://linktr.ee/jefferson_lopez_dev"
-          className="text-blue-500 underline-offset-4 hover:underline cursor-pointer flex items-center gap-1"
-        >
-          Jefferson Lopez
-          <ExternalLink className="w-4 h-4" />
-        </a>
-      </span>
+    <footer className="w-full bg-neutral-900 py-8 px-4 text-gray-300">
+      <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center">
+        <div className="mb-4 md:mb-0 text-center md:text-left">
+          {/* Logo del artista */}
+          <img
+            src="/vaquero.svg"
+            alt="Logo del Artista"
+            className="w-16 h-16 max-md:mx-auto mb-2 md:mb-0 md:mr-4 rounded-full"
+          />
+          <p className="text-sm flex items-center justify-center md:justify-start">
+            Sígueme en:
+            <span className="ml-2">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.youtube.com/@joserafaellopezosto8627"
+                className="text-blue-500 hover:underline flex items-center"
+              >
+                <Youtube className="w-4 h-4 mr-1" /> El Rondoneño
+              </a>
+            </span>
+            <span className="ml-4">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.facebook.com/profile.php?id=100063642931231&ref=bookmarks&paipv=0&eav=Afbjq1kp2k5fzYRMLwLGHEB2KLEFo-cEyEAaBjKjz1bey66a3RvY6Q_JyGWQJaXLFHM"
+                className="text-blue-500 hover:underline flex items-center"
+              >
+                <Facebook className="w-4 h-4 mr-1" /> Joropo Llano Y Leyenda
+              </a>
+            </span>
+            {/* Agrega más redes sociales según sea necesario */}
+          </p>
+          <p className="text-sm mt-2">
+            Página creada por{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://linktr.ee/jefferson_lopez_dev"
+              className="text-blue-500 hover:underline"
+            >
+              Jefferson Lopez
+            </a>
+          </p>
+        </div>
+        <div className="text-sm pt-5 text-center md:text-right">
+          <p>&copy; {new Date().getFullYear()} Jose Rafael Lopez Osto</p>
+          <p className="mt-2">Todos los derechos reservados.</p>
+        </div>
+      </div>
     </footer>
   );
 }
